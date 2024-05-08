6c24621a26e0573afeaea0ed685671a360cedc18
bwardak <baheer.wardak06@gmail.com>
2024-05-08 14:56:44 +0100

    Merged with main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,6 @@
 package org.example;
 
-import org.example.DeckOfCards.DeckOfCards;
-import org.example.DeckOfCards.DeckOfCardsUtils;
-import org.example.Game.Blackjack;
-import org.example.Utility.UserCreation;
-
-
 public class Main {
     public static void main(String[] args) {
-        DeckOfCardsUtils deckk = new DeckOfCardsUtils();
-        DeckOfCards deckcards = new DeckOfCards();
-        Blackjack blackjack = new Blackjack();
-        UserCreation user = new UserCreation();
-
-//        user.nameCreation();
-
-
-
-        blackjack.play();
     }
 }
