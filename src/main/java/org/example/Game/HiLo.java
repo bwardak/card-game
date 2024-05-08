9c561491ffbab0726e33b5e9997091da99ca6710
Michaela <michaelachan45@gmail.com>
2024-05-08 14:45:10 +0100

    hilo game done

--- src/main/java/org/example/Game/HiLo.java
+++ src/main/java/org/example/Game/HiLo.java
@@ -4,9 +4,7 @@ import org.example.DeckOfCards.DeckOfCards;
 import org.example.DeckOfCards.DeckOfCardsUtils;
 import org.example.Utility.InputScanner;
 
-import javax.smartcardio.Card;
-import java.sql.SQLOutput;
-
+import java.net.Socket;
 
 public class HiLo extends Game {
     private DeckOfCards deck;
@@ -18,99 +16,116 @@ public class HiLo extends Game {
         userInput = new InputScanner();
     }
 
-    private Game game = new Game("Higher or Lower", "1. You have to guess if the next card pulled will be higher, lower or the same card value than the current card."
-            + "\n 2. Ace is the lowest card"
-            + "\n 3. Type 'H' for higher, 'L' for lower and 'S' for same "
-            + "\n Game ends when you guess incorrectly") {
         @Override
         public void play() {
-            game.printTitle();
-            game.printRules();
-            do{
-                System.out.println("Shuffling Deck...");
-                deck.shuffleDeck();
-                deck.shuffleDeck();
-                System.out.println("The card is : " + deck.dealCard() + ". Higher, lower or same?");
+            System.out.println("Welcome to Higher or Lower");
+            printRules();
+            System.out.println("Have Fun!!");
+            System.out.println();
 
-                String userGuess = getUserInput().toString().toUpperCase();
-                if (userGuess.contains("H")){
-                    String nextCard = deck.dealCard();
-                    if (nextCard.equals()){
-                        System.out.println("You lose, they were the same. Too bad");
-                    } else if (getDeck().get > deck.dealCard()){
-                        System.out.println("Well done!");
-                    } else{
-                        System.out.println("You lose, the next card was lower. Try harder.");
-                    }
+            System.out.println("Shuffling Deck...");
+            deck.shuffleDeck();
+            deck.shuffleDeck();
+            boolean gameContinue;
+            do {
+                String currentCardString = deck.dealCard();
+                int currentCardValue;
+                switch (currentCardString.substring(0, currentCardString.length() - 1)) {
+                    case "A":
+                        currentCardValue = 1;
+                        break;
+                    case "J":
+                        currentCardValue = 11;
+                        break;
+                    case "Q":
+                        currentCardValue = 12;
+                        break;
+                    case "K":
+                        currentCardValue = 13;
+                        break;
+                    default:
+                        currentCardValue = Integer.parseInt(currentCardString.substring(0, currentCardString.length() - 1));
+                        break;
                 }
 
-            }
+                System.out.println("The card is : " + currentCardString + ". Higher, lower or same?");
+
+                String userGuess = userInput.useScanner().nextLine().toUpperCase();
+                String nextCardString = deck.dealCard();
+                int nextCardValue;
+                switch (nextCardString.substring(0, nextCardString.length() - 1)) {
+                    case "A":
+                        nextCardValue = 1;
+                        break;
+                    case "J":
+                        nextCardValue = 11;
+                        break;
+                    case "Q":
+                        nextCardValue = 12;
+                        break;
+                    case "K":
+                        nextCardValue = 13;
+                        break;
+                    default:
+                        nextCardValue = Integer.parseInt(nextCardString.substring(0, nextCardString.length() - 1));
+                        break;
+                }
+
+                gameContinue = false;
+                if (userGuess.contains("H")) {
+                    if (nextCardValue == currentCardValue) {
+                        System.out.println("You lose, they were the same. Too bad");
+                    } else if (nextCardValue > currentCardValue) {
+                        System.out.println("Well done!");
+                        gameContinue = true;
+                    } else if (nextCardValue < currentCardValue) {
+                        System.out.println("You lose, the next card was " + nextCardString + ". Try harder.");
+                    }
+                }
+                if (userGuess.contains("L")) {
+                    if (nextCardValue == currentCardValue) {
+                        System.out.println("You lose, they were the same. Too bad");
+                    } else if (nextCardValue < currentCardValue) {
+                        System.out.println("Well done!");
+                        gameContinue = true;
+                    } else if (nextCardValue > currentCardValue) {
+                        System.out.println("You Lose, the next card was " + nextCardString + ". Try again");
+                    }
+                }
+                if (userGuess.contains("S")) {
+                    if (nextCardValue == currentCardValue) {
+                        System.out.println("Well done! They are the same value.");
+                        gameContinue = true;
+                    } else if (nextCardValue > currentCardValue) {
+                        System.out.println("You lose, the next card was " + nextCardString);
+                    } else if (nextCardValue < currentCardValue) {
+                        System.out.println("You lose, the next card was " + nextCardString);
+                    }
+                }
+            } while (gameContinue);
+            playAgain();
         }
-
-        @Override
-        public boolean playAgain() {
-            return false;
-        }
-    };
-
-
-
-    public DeckOfCards getDeck() {
-        return deck;
-    }
-
-    public void setDeck(DeckOfCards deck) {
-        this.deck = deck;
-    }
-
-    public InputScanner getUserInput() {
-        return userInput;
-    }
-
-    public void setUserInput(InputScanner userInput) {
-        this.userInput = userInput;
-    }
-
-    @Override
-    public void play() {
-
-    }
 
     @Override
     public boolean playAgain() {
+        System.out.println("Play again? y/n");
+        String input = userInput.useScanner().nextLine().toUpperCase();
+        if(input.equals("Y")){
+            play();
+        } else if (input.equals("N")){
+            System.out.println("Thanks for playing");
+        }
         return false;
     }
 
-    // rules of the game
-    // 1. ace is lowest card
-    // 2. jack, queen, king
-    // 3. if cards at the same number
-        //  either
-            // game ends
-            // dependant on suit strength
-                // diamond, club, heart, spade // so much harder.
-            // have a third option of high, low or same.
+
+    public void printRules(){
+            System.out.println("1. You have to guess if the next card pulled will be higher, lower or the same card value than the current card. \n"
+                    + "2. Ace is the lowest card \n"
+                    + "3. Type 'H' for higher, 'L' for lower and 'S' for same \n"
+                    + "4. Game ends when you guess incorrectly");
+        }
+
+};
 
 
-//    public void playHiLo() {
-//        // pull first card
-//        //user input of if they think its higher or lower (higher || lower )
-//
-//        System.out.println();
-//    }
-public static void main(String[] args){
-        DeckOfCards deck1 = new DeckOfCards();
-        InputScanner scanner = new InputScanner();
-
-    System.out.println("Shuffling Deck...");
-    deck1.shuffleDeck();
-    deck1.shuffleDeck();
-    System.out.println("The card is : " + deck1.dealCard() + ". Higher, lower or same?");
-
-
-
-
-
-}
-
-}
